416f077d90fa62ba57c47fd411a44ae70b74c8bb
Victor Schweitz <victorschweitz@hotmail.com>
2024-08-15 19:42:23 +0200

    FIxing topic cards for first semester

--- src/topic_cards_data/first_semester_topic_cards.js
+++ src/topic_cards_data/first_semester_topic_cards.js
@@ -1,68 +1,68 @@
 export const firstSemesterTopicCards =
 [
     {
-        cardURL: 'first_semester_topics/functions',
+        cardURL: 'topics/first_semester_topics/functions',
         cardFrontText: 'Funktioner',
         cardBackText: 'function myFunction()',
         topicNumber: 1
     },
     {
-        cardURL: 'first_semester_topics/variables_constants',
+        cardURL: 'topics/first_semester_topics/variables_constants',
         cardFrontText: 'Variable & Konstante',
         cardBackText: 'var, let, const',
         topicNumber: 2,
     },
     {
-        cardURL: 'first_semester_topics/selectors',
+        cardURL: 'topics/first_semester_topics/selectors',
         cardFrontText: 'Selectors',
         cardBackText: 'Klasser, id\'er & elementer',
         topicNumber: 3
     },
     {
-        cardURL: 'first_semester_topics/events',
+        cardURL: 'topics/first_semester_topics/events',
         cardFrontText: 'Eventlisteners',
         cardBackText: '.addEventListener()',
         topicNumber: 4
     },
     {
-        cardURL: 'first_semester_topics/classes',
+        cardURL: 'topics/first_semester_topics/classes',
         cardFrontText: 'Klassemanipulation',
         cardBackText: '.add(), .remove(), .toggle()',
         topicNumber: 5
     },
    
     {
-        cardURL: 'first_semester_topics/timers',
+        cardURL: 'topics/first_semester_topics/timers',
         cardFrontText: 'Timers',
         cardBackText: 'setInterval()/setTimeout()',
         topicNumber: 6
     },
     {
-        cardURL: 'first_semester_topics/random',
+        cardURL: 'topics/first_semester_topics/random',
         cardFrontText: 'Tilfældighed',
         cardBackText: 'Math.random()',
         topicNumber: 7
     },
     {
-        cardURL: 'first_semester_topics/operands',
+        cardURL: 'topics/first_semester_topics/operands',
         cardFrontText: 'Sammenlignende operatorer',
         cardBackText: '<, >, <=, >=, ||, &&, =, ==',
         topicNumber: 8
     },
     {
-        cardURL: 'first_semester_topics/conditions',
+        cardURL: 'topics/first_semester_topics/conditions',
         cardFrontText: 'Forhold',
         cardBackText: 'if/else',
         topicNumber: 9
     },
     {
-        cardURL: 'first_semester_topics/console',
+        cardURL: 'topics/first_semester_topics/console',
         cardFrontText: 'Konsollen',
         cardBackText: 'console.log()',
         topicNumber: 10
     },
     {
-        cardURL: 'first_semester_topics/sound',
+        cardURL: 'topics/first_semester_topics/sound',
         cardFrontText: 'Lyd',
         cardBackText: '.play(), .pause(), .muted, \n, .volume, .duration, .currentTime',
         topicNumber: 11
