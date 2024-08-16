80305d8828887d1e86c11016dd0c4f072d6e57a3
Victor Schweitz <victorschweitz@hotmail.com>
2024-08-16 13:40:02 +0200

    Fixed some more

--- src/topic_cards_data/second_semester_topic_cards.js
+++ src/topic_cards_data/second_semester_topic_cards.js
@@ -1,50 +1,50 @@
 export const secondSemesterTopicCards =
 [
     {
-        cardURL: 'second_semester_topics/arrays',
+        cardURL: 'arrays',
         cardFrontText: 'Arrays',
         cardBackText: 'const arr = []',
         topicNumber: 1
     },
     {
-        cardURL: 'second_semester_topics/objects',
+        cardURL: 'objects',
         cardFrontText: 'Objekter',
         cardBackText: 'const obj = {}',
         topicNumber: 2,
     },
     {
-        cardURL: 'second_semester_topics/fetch',
+        cardURL: 'fetch',
         cardFrontText: 'Fetch',
         cardBackText: 'fetch()',
         topicNumber: 3
     },
     {
-        cardURL: 'second_semester_topics/query_selector_all',
+        cardURL: 'query_selector_all',
         cardFrontText: 'Selector',
         cardBackText: '.querySelectorAll()',
         topicNumber: 4
     },
     {
-        cardURL: 'second_semester_topics/for_each',
+        cardURL: 'for_each',
         cardFrontText: 'Loop',
         cardBackText: '.forEach()',
         topicNumber: 5
     },
    
     {
-        cardURL: 'second_semester_topics/template',
+        cardURL: 'template',
         cardFrontText: 'Template',
         cardBackText: '.cloneNode(), .cloneNode(true),\n .append()/.appendChild()',
         topicNumber: 6
     },
     {
-        cardURL: 'second_semester_topics/url_parameters',
+        cardURL: 'url_parameters',
         cardFrontText: 'URL-parametre',
         cardBackText: 'URLSearchParams()',
         topicNumber: 7
     },
     {
-        cardURL: 'second_semester_topics/attributes',
+        cardURL: 'attributes',
         cardFrontText: 'Attributter',
         cardBackText: '.getAttribute()/.setAttribute()',
         topicNumber: 8
